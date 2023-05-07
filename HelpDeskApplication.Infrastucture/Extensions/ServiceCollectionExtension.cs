fba62173ab93a918029b86b067034eb9b263d9e0
micha <harasim1999@gmail.pl>
2023-05-07 23:58:57 +0200

    HDA-5

---
+++ HelpDeskApplication.Infrastucture/Extensions/ServiceCollectionExtension.cs
@@ -0,0 +1,21 @@
+﻿using HelpDeskApplication.Infrastucture.Database;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelpDeskApplication.Infrastucture.Extensions
+{
+    public static class ServiceCollectionExtension
+    {
+        public static void AddInfrastucture(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<HelpDeskApplicationDbContext>(options => options.UseSqlServer(
+                configuration.GetConnectionString("HelpDeskApplication")));
+        }
+    }
+}
