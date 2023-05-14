d488126cc00c83e36706527da87842b0709a5530
micha <harasim1999@gmail.pl>
2023-05-14 19:39:04 +0200

    HDA-18

---
+++ HelpDeskApplication/Extenions/ControllerExtenions.cs
@@ -0,0 +1,16 @@
+﻿using HelpDeskApplication.Models;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory.Database;
+
+namespace HelpDeskApplication.Extenions
+{
+    public static class ControllerExtenions
+    {
+        public static void SetNotification(this Controller controller, string type, string message)
+        {
+            var notification = new Notification(type, message);
+            controller.TempData["Notification"] = JsonConvert.SerializeObject(notification);
+        }
+    }
+}
---
+++ HelpDeskApplication/Models/Notification.cs
@@ -0,0 +1,14 @@
+﻿namespace HelpDeskApplication.Models
+{
+    public class Notification
+    {
+        public Notification(string type, string meesage)
+        {
+            Type = type;
+            Meesage = meesage;
+        }
+
+        public string Type { get; set; }
+        public string Meesage { get; set; }
+    }
+}
