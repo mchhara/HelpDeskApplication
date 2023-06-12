c4de9696c6500db9dd34e0c3b0e46a661dee7e76
micha <harasim1999@gmail.pl>
2023-06-12 18:35:59 +0200

    HDA-43

---
+++ HelpDeskApplication.Application/ApplicationUser/Commands/DeleteUser/DeleteUserCommandHandler.cs
@@ -0,0 +1,24 @@
+﻿using HelpDeskApplication.Application.Ticket.Commands.DeleteTicket;
+using HelpDeskApplication.Domain.Interfaces;
+using MediatR;
+
+namespace HelpDeskApplication.Application.ApplicationUser.Commands.DeleteUser
+{
+    public class DeleteUserCommandHandler : IRequestHandler<DeleteUserCommand>
+    {
+        private readonly IUserRepository _userRepository;
+
+        public DeleteUserCommandHandler(IUserRepository userRepository)
+        {
+            _userRepository = userRepository;
+
+        }
+
+        public async Task<Unit> Handle(DeleteUserCommand request, CancellationToken cancellationToken)
+        {
+            await _userRepository.DeleteUser(request.UserName);
+
+            return Unit.Value;
+        }
+    }
+}
